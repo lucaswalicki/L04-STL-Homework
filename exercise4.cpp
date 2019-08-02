40fed8b5782a8d604d6a81342cb8e92f6ddce402
Lucas W <lukaszwalicki.walus@gmail.com>
2019-08-02 11:23:08 +0200

    Execrise 4 - vector

--- exercise4.cpp
+++ exercise4.cpp
@@ -1,44 +1,19 @@
 #include <iostream>
-#include <list>
+#include <vector>
 #include <iterator>
 #include <numeric>
 
-void printContent(std::list<int> a)
+void printCapacity(std::vector<int> a)
 {
-    for(auto elements:a)
-    {
-        std::cout << elements << " ";
-    }
-    std::cout << std::endl;
-}
-
-void printSize(std::list<int>a)
-{
-    std::cout << "size: " << a.size() << std::endl;
+    std::cout<< a.size()<<std::endl;
+    std::cout<< a.capacity() << std::endl;
 }
 
 int main()
 {
-    std::list<int> list{};
-    printSize(list);
-    auto it = list.begin();
-    for(int i=1; i<=1000000; i++)
-    {
-        list.insert(it, 1, i);
-        it++;
-    }
-    printSize(list);
-    auto it2 = list.begin();
-    std::advance(it2, 499999);
-    std::cout << "500000th element = " << *it2 << std::endl;
+    std::vector<int> v(1000000);
+    std::iota(v.begin(), v.end(), 1);
+    printCapacity(v);
+    std::cout << "Displaying 500000th element of vector v: " << v[499999] << std::endl;
     return 0;
 }
-
-/////////////////////////////////////////////////////////////
-// alternative fill method
-//
-// std::list<int> list(1000000);
-// std::itoa(list.begin(), list.end(), 1);
-
-
-    
