7b3d140aba22052301d87c18dce8859f5d26e41d
Lucas W <lukaszwalicki.walus@gmail.com>
2019-08-02 16:25:47 +0200

    Exercise 6 - cleanup

--- exercise6.cpp
+++ exercise6.cpp
@@ -2,18 +2,6 @@
 #include <forward_list>
 #include <iterator>
 
-/*
-void printSize(int x, int y)
-{
-    int flistSize = 0;
-    for (auto i=x; i!= y; ++i)
-    {
-        ++flistSize;
-    }
-    std::cout << flistSize << std::endl;
-}
-*/
-
 int main()
 {
     std::forward_list<int> flist{2, 34, 7, 87, 43, 9, 0, 2};
@@ -27,5 +15,5 @@ int main()
     std::cout << flistSize << std::endl;
     std::advance(itFront, 4);
     std::cout << "5th element = " << *itFront << std::endl;
-    std:: cout << "distance = " << std::distance(flist.begin(), itFront) << std::endl;
+    std:: cout << "Distance = " << std::distance(flist.begin(), itFront) << std::endl;
 }
