7d6fb344e685554bfb16dad1dde9bc20975c43ba
TheDude-cpu <dudek.u.sylwia@gmail.com>
2019-08-04 11:45:19 +0200

    change for ex6

--- exercise6.cpp
+++ exercise6.cpp
@@ -4,16 +4,12 @@
 
 int main()
 {
-    std::forward_list<int> flist{2, 34, 7, 87, 43, 9, 0, 2};
-    auto itFront = flist.begin();
-    auto itBack = flist.end();
-    int flistSize = 0;
-    for (auto i=itFront; i!= itBack; ++i)
-    {
-        ++flistSize;
-    }
-    std::cout << flistSize << std::endl;
-    std::advance(itFront, 4);
-    std::cout << "5th element = " << *itFront << std::endl;
-    std:: cout << "Distance = " << std::distance(flist.begin(), itFront) << std::endl;
+    std::forward_list<int> flist = {2, 34, 7, 87, 43, 9, 0, 2};
+    auto itFront = begin(flist);
+    auto itBack = end(flist);
+    std::cout << "size: " << std::distance(itFront,itBack) <<std::endl;
+    auto iter5 = flist.begin();
+    std::advance(iter5, 4);
+    std::cout << "5th element = " << *iter5 << std::endl;
+    std:: cout << "Distance = " << std::distance(itFront, iter5) << std::endl;
 }
